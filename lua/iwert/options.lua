f5b13e18e8c2438eff38f8ddaeb714678e525a41
Iwert <iwert.m@gmail.com>
2023-02-08 11:22:05 +0100

    Filetype recognition for Terraform

--- lua/iwert/options.lua
+++ lua/iwert/options.lua
@@ -53,3 +53,10 @@ if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
+
+-- Filetype recognition for Terraform
+vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
+vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tf,*.tfvars set filetype=terraform]])
+vim.cmd([[autocmd BufRead,BufNewFile *.tfstate,*.tfstate.backup set filetype=json]])
