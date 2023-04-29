8710d68fdb9e68e36ab3e4c0016456fb3222da15
Iwert <iwert.m@gmail.com>
2023-04-29 20:54:16 +0200

    Show folded lines

--- lua/iwert/ufo.lua
+++ lua/iwert/ufo.lua
@@ -11,29 +11,46 @@ vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == clo
 vim.keymap.set('n', '<C-s>', function()
     local winid = require('ufo').peekFoldedLinesUnderCursor()
     if not winid then
-        -- choose one of coc.nvim and nvim lsp
-        vim.fn.CocActionAsync('definitionHover') -- coc.nvim
         vim.lsp.buf.hover()
     end
 end)
-require('ufo').setup()
---
 
--- Option 3: treesitter as a main provider instead
--- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
--- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
---[[ use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'} ]]
---[[ require('ufo').setup({ ]]
---[[     provider_selector = function(bufnr, filetype, buftype) ]]
---[[         return {'treesitter', 'indent'} ]]
---[[     end ]]
---[[ }) ]]
---
+local handler = function(virtText, lnum, endLnum, width, truncate)
+    local newVirtText = {}
+    local suffix = ('  %d '):format(endLnum - lnum)
+    local sufWidth = vim.fn.strdisplaywidth(suffix)
+    local targetWidth = width - sufWidth
+    local curWidth = 0
+    for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+        else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, {chunkText, hlGroup})
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+        end
+        curWidth = curWidth + chunkWidth
+    end
+    table.insert(newVirtText, {suffix, 'MoreMsg'})
+    return newVirtText
+end
 
--- Option 4: disable all providers for all buffers
--- Not recommend, AFAIK, the ufo's providers are the best performance in Neovim
---[[ require('ufo').setup({ ]]
---[[     provider_selector = function(bufnr, filetype, buftype) ]]
---[[         return '' ]]
---[[     end ]]
---[[ }) ]]
+-- global handler
+-- `handler` is the 2nd parameter of `setFoldVirtTextHandler`,
+-- check out `./lua/ufo.lua` and search `setFoldVirtTextHandler` for detail.
+require('ufo').setup({
+    fold_virt_text_handler = handler
+})
+
+-- buffer scope handler
+-- will override global handler if it is existed
+-- local bufnr = vim.api.nvim_get_current_buf()
+-- require('ufo').setFoldVirtTextHandler(bufnr, handler)
