0a3469f3feb693b5e84939361f26754a4960bdca
Iwert M <iwert.m@gmail.com>
2025-10-10 15:42:53 +0200

    add codecompanion

---
+++ lua/plugins/codeCompanion.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "codecompanion" },
+  },
+  {
+    "nvim-mini/mini.diff",
+    config = function()
+      local diff = require("mini.diff")
+      diff.setup({
+        -- Disabled by default
+        source = diff.gen_source.none(),
+      })
+    end,
+  },
+}
